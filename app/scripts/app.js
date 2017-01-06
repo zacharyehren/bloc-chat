1decda0da22155585e318155045069c6fd0a4736
Ben Neely <ben.neely@kount.com>
2017-01-05 22:14:18 -0700

    Missing ();

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,21 @@
 (function() {
   function config($stateProvider, $locationProvider) {
-   
+
     $locationProvider
       .html5Mode({
       enabled: true,
       requireBase: false
     });
-    
+
     $stateProvider
       .state('home', {
       url: '/',
       controller: 'RoomsCtrl as rooms',
       templateUrl: '/templates/home.html'
     });
-    
+
   }
   angular
   .module('blocChat', ['ui.router', 'firebase'])
   .config(config);
-})
+})();
